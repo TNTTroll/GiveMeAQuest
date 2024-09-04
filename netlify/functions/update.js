67ab568de7164e041fd7155aede1928df8154875
TNTTroll <MyBlocks@mail.ru>
2024-09-04 22:23:57 +0300

    Try2

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -1,7 +1,16 @@
-//const axios = require("axios").default;
-
-
-exports.handler = async (event) => {
+/*exports.handler = async (event) => {
   console.log("Received an update from Telegram!", event.body);
   return { statusCode: 200 };
+};
+*/
+
+const axios = require("axios").default;
+
+module.exports = async (chat_id, text) => {
+    await axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
+        chat_id,
+        text,
+    });
+
+    return true;
 };
