9d66d59122ccc1b100317ac7d36847e3a0e9ac9c
TNTTroll <MyBlocks@mail.ru>
2024-09-04 22:39:34 +0300

    Try5

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -2,6 +2,7 @@ const sendMessage = require("../../sendMessage");
 
 exports.handler = async (event) => {
   const { message } = JSON.parse(event.body);
+  console.log(message);
   await sendMessage(message.chat.id, "I got your message!");
   return { statusCode: 200 };
 };
