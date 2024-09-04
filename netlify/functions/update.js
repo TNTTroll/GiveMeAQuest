6ecc808652779e55d9bf8c5e9362d3316c6db191
TNTTroll <MyBlocks@mail.ru>
2024-09-04 22:44:03 +0300

    Try6

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -1,8 +1,43 @@
+// --- Imports
 const sendMessage = require("../../sendMessage");
 
+
+// --- Variables
+const users = [];
+
+
+// --- Main
 exports.handler = async (event) => {
-  const { message } = JSON.parse(event.body);
-  console.log(message);
-  await sendMessage(message.chat.id, "I got your message!");
-  return { statusCode: 200 };
+    const { msg } = JSON.parse(event.body);
+
+    let user = msg.from;
+
+    let exist = false;
+    for (let i = 0; i < users.length; i++)
+        if (users[i].id == user.id) {
+            exist = true;
+
+            await sendMessage(msg.chat.id, 
+                `С возвращением, <b>${users[i].name}</b>!`);
+        }
+
+    if (!exist) {
+        let newUser = new User(user.id, user.first_name);
+        users.push(newUser);
+
+        await sendMessage(msg.chat.id, 
+            `Добро пожаловать в Гильдию, <b>${newUser.name}</b>!`);
+    }
+
+    return { statusCode: 200 };
 };
+
+
+// --- Classes
+class User {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+        this.points = 0;
+    }
+}
