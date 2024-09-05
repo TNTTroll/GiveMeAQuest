f6aaff0d8f0e6fc1814b0de818c4c9dccab66532
TNTTroll <MyBlocks@mail.ru>
2024-09-05 21:44:07 +0300

    DEFAULT

--- sendMessage.js
+++ sendMessage.js
@@ -1,8 +1,6 @@
 const axios = require("axios").default;
 
 module.exports = async (chat_id, text) => {
-  console.log(text);
-
   await axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
     chat_id,
     text,
